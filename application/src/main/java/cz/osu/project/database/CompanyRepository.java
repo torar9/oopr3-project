d28699e8819330e5477800e4648fdd74a40c94c3
torar9 <silber.tomas@outlook.cz>
2020-11-06 19:15:47 +0100

    Added repositories

---
+++ application/src/main/java/cz/osu/project/database/CompanyRepository.java
@@ -0,0 +1,6 @@
+package cz.osu.project.database;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
+}
---
+++ application/src/main/java/cz/osu/project/database/ExpeditionRepository.java
@@ -0,0 +1,6 @@
+package cz.osu.project.database;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExpeditionRepository extends CrudRepository<Expedition, Integer> {
+}
