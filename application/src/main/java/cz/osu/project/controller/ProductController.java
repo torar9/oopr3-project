08e4a4d90da4a5564862c4c7298a316c81cd8749
torar9 <silber.tomas@outlook.cz>
2020-11-11 00:46:23 +0100

    Added /products

--- application/src/main/java/cz/osu/project/controller/ProductController.java
+++ application/src/main/java/cz/osu/project/controller/ProductController.java
@@ -1,11 +1,14 @@
 package cz.osu.project.controller;
 
+import cz.osu.project.database.entity.Product;
 import cz.osu.project.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller("/product")
 public class ProductController {
@@ -21,26 +24,16 @@ public class ProductController {
     StockItemService stockService;
 
     @GetMapping("/products")
-    public String getProducts(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String getProducts(Model model) {
+        List<Product> products = productService.getAll();
+        model.addAttribute("products", products);
+
         return "products";
     }
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
-    }
-
-    @GetMapping("/newItem")
-    public String newItem(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        /*
-        contactService.create("email@tom.cz", "123456789", "12485546");
-        addressServiceService.create("Majčovičkovičkovova", "101", "56651", "Ostrava", "Slovakia");
-        companyService.create("Company s.r.o.", 1, 1);
-        productService.create("jmeno", "popis");
-        stockService.create(10, 350.0, 1, 1);*/
         return "greeting";
     }
 }
