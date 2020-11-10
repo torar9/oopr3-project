8c7320ae390eca1b698fe8b2ac41609592000ff5
torar9 <silber.tomas@outlook.cz>
2020-11-10 22:14:47 +0100

    Use bootstrap

--- application/src/main/java/cz/osu/project/controller/ProductController.java
+++ application/src/main/java/cz/osu/project/controller/ProductController.java
@@ -21,6 +21,12 @@ public class ProductController {
     StockItemService stockService;
 
     @GetMapping("/products")
+    public String getProducts(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "products";
+    }
+
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
