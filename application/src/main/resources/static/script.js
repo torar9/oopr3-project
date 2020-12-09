8a0b34dd4d87594620acfced2c9bfb8f3eddaf76
torar9 <silber.tomas@outlook.cz>
2020-12-09 14:16:48 +0100

    Added dialog popup when deleting items or changing expedition status

--- application/src/main/resources/static/script.js
+++ application/src/main/resources/static/script.js
@@ -3,3 +3,24 @@ jQuery(document).ready(function($) {
         window.location = $(this).data("href");
     });
 });
+
+$(document).on('click', ':not(form)[data-confirm]', function(e){
+    if(!confirm($(this).data('confirm'))){
+        e.stopImmediatePropagation();
+        e.preventDefault();
+    }
+});
+
+$(document).on('submit', 'form[data-confirm]', function(e){
+    if(!confirm($(this).data('confirm'))){
+        e.stopImmediatePropagation();
+        e.preventDefault();
+    }
+});
+
+$(document).on('input', 'select', function(e){
+    var msg = $(this).children('option:selected').data('confirm');
+    if(msg != undefined && !confirm(msg)){
+        $(this)[0].selectedIndex = 0;
+    }
+});
